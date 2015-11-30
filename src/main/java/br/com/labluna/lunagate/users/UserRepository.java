adbe9a373aba458bc59530a633f465e7d76f9491
Diego Rubin <rubin.diego@gmail.com>
2015-11-30 10:56:37 -0200

    changing java/sprint to python/django.

--- src/main/java/br/com/labluna/lunagate/users/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.labluna.lunagate.users;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
